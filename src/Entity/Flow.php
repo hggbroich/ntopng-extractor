4ab32dc79faf952e51b5e01ee16038702d78892e
Marcel Marnitz <marcelmarnitz@googlemail.com>
2026-02-18 15:15:58 +0100

    Fixes deprecation + correctly use unique idx

--- src/Entity/Flow.php
+++ src/Entity/Flow.php
@@ -8,9 +8,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
-#[ORM\Table(uniqueConstraints: [
-    new ORM\UniqueConstraint(columns: ['hashId', 'key'])
-])]
+#[ORM\UniqueConstraint(columns: ['hash_id', 'key'])]
 class Flow {
     #[ORM\Id]
     #[ORM\GeneratedValue]
