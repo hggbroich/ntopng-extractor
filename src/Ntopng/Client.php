b6cdd24326d7ae3858532df663b99fc392b2856c
Marcel Marnitz <marcelmarnitz@googlemail.com>
2026-02-19 13:36:33 +0100

    Fix wrong UTF-8 characters in JSON

--- src/Ntopng/Client.php
+++ src/Ntopng/Client.php
@@ -4,29 +4,34 @@ namespace App\Ntopng;
 
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 readonly class Client {
     public function __construct(private HttpClientInterface $ntopngClient, private SerializerInterface $serializer) {
 
     }
 
+    private function getJsonFromResponse(ResponseInterface $response): string {
+        return mb_convert_encoding($response->getContent(), 'UTF-8', 'UTF-8');
+    }
+
     public function getInterfaces(): InterfacesResponse {
         $json = $this->ntopngClient->request('GET', '/lua/rest/v2/get/ntopng/interfaces.lua');
-        return $this->serializer->deserialize($json->getContent(), InterfacesResponse::class, 'json');
+        return $this->serializer->deserialize($this->getJsonFromResponse($json), InterfacesResponse::class, 'json');
     }
 
     public function getFlows(int $interfaceId, int $page = 1, int $perPage = 100): ActiveFlowsResponse {
         $json = $this->ntopngClient->request('GET', '/lua/rest/v2/get/flow/active.lua?ifid=' . $interfaceId . '&currentPage=' . $page . '&perPage=' . $perPage);
-        return $this->serializer->deserialize($json->getContent(), ActiveFlowsResponse::class, 'json');
+        return $this->serializer->deserialize($this->getJsonFromResponse($json), ActiveFlowsResponse::class, 'json');
     }
 
     public function getActiveFlowsList(int $interfaceId, int $start = 0, int $length = 10000): ActiveFlowListResponse {
         $json = $this->ntopngClient->request('GET', '/lua/rest/v2/get/flow/active_list.lua?ifid=' . $interfaceId . '&start=' . $start . '&length=' . $length);
-        return $this->serializer->deserialize($json->getContent(), ActiveFlowListResponse::class, 'json');
+        return $this->serializer->deserialize($this->getJsonFromResponse($json), ActiveFlowListResponse::class, 'json');
     }
 
     public function getLocalHosts(int $interfaceId, int $page = 1, int $perPage = 100): ActiveHostsResponse {
         $json = $this->ntopngClient->request('GET', '/lua/rest/v2/get/host/active.lua?ifid=' . $interfaceId . '&mode=local');
-        return $this->serializer->deserialize($json->getContent(), ActiveHostsResponse::class, 'json');
+        return $this->serializer->deserialize($this->getJsonFromResponse($json), ActiveHostsResponse::class, 'json');
     }
 }
