83b4ba3c11b5a764208e2280d9e20275dbfbe348
Marcel Marnitz <marcelmarnitz@googlemail.com>
2026-05-07 19:35:22 +0200

    Store errors from StoreFlowDetailsMessageHandler in log instead of retrying

--- src/MessageHandler/StoreFlowDetailsMessageHandler.php
+++ src/MessageHandler/StoreFlowDetailsMessageHandler.php
@@ -5,31 +5,39 @@ namespace App\MessageHandler;
 use App\Entity\Flow;
 use App\Message\StoreFlowDetailsMessage;
 use Doctrine\ORM\EntityManagerInterface;
+use Error;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Throwable;
 
 #[AsMessageHandler]
 readonly class StoreFlowDetailsMessageHandler {
 
     public function __construct(
-        private EntityManagerInterface $entityManager
+        private EntityManagerInterface $entityManager,
+        private LoggerInterface $logger
     ) { }
 
     public function __invoke(StoreFlowDetailsMessage $flowMessage): void {
-        $flow = $flowMessage->flowEntry;
+        try {
+            $flow = $flowMessage->flowEntry;
 
-        $storedFlow = $this->entityManager->getRepository(Flow::class)->findOneBy([
-            'key' => $flow->key,
-            'hashId' =>  $flow->hashId
-        ]);
+            $storedFlow = $this->entityManager->getRepository(Flow::class)->findOneBy([
+                'key' => $flow->key,
+                'hashId' => $flow->hashId
+            ]);
 
-        if($storedFlow === null) {
-            throw new FlowNotFoundException($flow->key, $flow->hashId);
+            if ($storedFlow === null) {
+                throw new FlowNotFoundException($flow->key, $flow->hashId);
+            }
+
+            $storedFlow->info = $flow->info;
+            $storedFlow->localMac = $flow->macAddress;
+
+            $this->entityManager->persist($storedFlow);
+            $this->entityManager->flush();
+        } catch (Throwable $exception) {
+            $this->logger->error($exception->getMessage());
         }
-
-        $storedFlow->info = $flow->info;
-        $storedFlow->localMac = $flow->macAddress;
-
-        $this->entityManager->persist($storedFlow);
-        $this->entityManager->flush();
     }
 }
